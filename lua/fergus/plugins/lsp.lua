3d2e3840c19e3e1a53dc9649c5e89882931fae8a
FergusJJ <fergus2004johnson@gmail.com>
2024-12-19 22:30:04 +0000

    feat: add solhint linter

--- lua/fergus/plugins/lsp.lua
+++ lua/fergus/plugins/lsp.lua
@@ -192,6 +192,7 @@ return {
                 formatting = {
                   provider = "prettierd",
                 },
+                linter = "solhint"
               },
             },
           }
