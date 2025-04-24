47aa2440403f597fb023a78e855c238c7d902ef9
FergusJJ <fergus2004johnson@gmail.com>
2025-04-24 17:37:38 +0100

    Add dap,dapui for xcode, change harpoon kbs

--- lua/fergus/plugins/harpoon.lua
+++ lua/fergus/plugins/harpoon.lua
@@ -1,24 +1,23 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function ()
-        local harpoon = require("harpoon")
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
 
-        harpoon:setup()
+    harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-a>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-z>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-\\>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-q>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-a>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-z>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
-        vim.keymap.set("n", "<leader><C-q>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader><C-a>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader><C-z>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader><C-\\>", function() harpoon:list():select(4) end)
-
-    end
+    vim.keymap.set("n", "<leader><C-q>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<leader><C-a>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader><C-z>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():select(4) end)
+  end
 }
--- lua/fergus/remap.lua
+++ lua/fergus/remap.lua
@@ -10,34 +10,20 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-
 --paste without overwriting register
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>D", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
---vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
---vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
---vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---vim.keymap.set("n", "<leader>XX", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- this is interesting, might forget to use it
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
